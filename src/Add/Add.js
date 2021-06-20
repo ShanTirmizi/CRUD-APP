6083cf9346e10f1bd2cd1dc536f88c3039d9516f
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2021-06-20 16:35:34 +0100

    Adding functionality works

--- src/Add/Add.js
+++ src/Add/Add.js
@@ -1,15 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useReducer, useState } from 'react'
+import { Link, Switch } from 'react-router-dom'
+
+const ACTIONS = {
+    ADD_LIST_ITEM: 'add-list-item'
+}
+
+const reducer = (lists, action) => {
+    switch (action.type) {
+        case ACTIONS.ADD_LIST_ITEM:
+            return [...lists, newList(action.payload.name)] 
+    }
+}
+
+const newList = (name) => {
+    return { id: Date.now(), name: name, complete: false }
+}
 
 const Add = () => {
+    const [lists, dispatch] = useReducer(reducer, [])
+    const [name, setName] = useState('')
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        dispatch({ type: ACTIONS.ADD_LIST_ITEM, payload: {name: name} })
+        setName('')
+    }
+
+    console.log(lists)
     return (
         <div>        
-            <from>
+            <form onSubmit={handleChange}>
                 <label for="fname">Name</label>
-                <input type="text" id="fname" name="fname" value="John"/>
+                <input type="text" id="fname" name="fname" value={name} onChange={e => setName(e.target.value)}/>
                 <input type="submit" value="Submit" />
                 <Link to='/'>Cancel</Link>
-            </from>
+            </form>
         </div>
     )
 }
