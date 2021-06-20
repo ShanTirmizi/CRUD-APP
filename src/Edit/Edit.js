b9b462e7211d7cffce4e2535a1e6bd1470544870
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2021-06-20 13:50:34 +0100

    Basic setup done

--- src/Edit/Edit.js
+++ src/Edit/Edit.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Edit = () => {
     return (
         <div>
-            Edit
+            <from>
+                <label for="fname">Name</label>
+                <input type="text" id="fname" name="fname" value="John"/>
+                <input type="submit" value="Edit Name" />
+                <Link to='/'>Cancel</Link>
+            </from>
         </div>
     )
 }
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import List from '../List/List'
 
 const Home = () => {
     return (
         <div>
             Home
+            <List />
         </div>
     )
 }
